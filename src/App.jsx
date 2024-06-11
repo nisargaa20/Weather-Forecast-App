787142abac84238e3e8cbf70e4b67717c47b3e64
nisargaa20 <125184228+nisargaa20@users.noreply.github.com>
2024-06-11 20:42:20 +0530

    api implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
-import React from 'react'
+
+
+import React, { useEffect } from 'react';
+import getFormattedWeatherData from "./services/weather-Service";
 
 const App = () => {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData( { q: "berlin" });
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-grey-400 from-cyan-600 to-blue-700">
     <TopButtons />
